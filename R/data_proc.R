02419258003925726abbbdd47fd9ad0e3dc83241
Doug Sponsler <sponslerdb@gmail.com>
2018-07-16 13:29:05 -0400

    improved annotations

--- R/data_proc.R
+++ R/data_proc.R
@@ -1,46 +1,48 @@
-#' Load BroodMinder SQLite database and extract logged readings into a tibble
+#' \code{load_bm} loads a BroodMinder SQLite database and extracts logged readings into a tibble.
 #'
 #' @param FilePath Path to BroodMinder SQLite database
 #' @param tz String identifying time zone
 #' @param ftrim String "YYYY-mm-dd" indicating front date cutoff
 #' @param btrim String "YYYY-mm-dd" indicating back date cutoff
+#' @param sep A string denoting the separator between the site and hive number in the HiveName field; assumes HiveName follows format site % sep % hive#
 #' @return A tibble containing selected fields and trimmed to front and back cutoff dates
 #' @export
-load_db <- function(FilePath, tz, ftrim, btrim) {
-  data_sqlite <- RSQLite::dbConnect(RSQLite::SQLite(), FilePath)
-  dat <- RSQLite::dbGetQuery(data_sqlite, "SELECT * FROM BroodMinderLogReading")
-  RSQLite::dbDisconnect(data_sqlite)
-  hnames <- get_names(FilePath)
+load_bm <- function(FilePath, tz, ftrim, btrim, sep = "\\.") {
+  data_sqlite <- RSQLite::dbConnect(RSQLite::SQLite(), FilePath) # opens db connection
+  dat <- RSQLite::dbGetQuery(data_sqlite, "SELECT * FROM BroodMinderLogReading") # extracts BroodMinderLogReading sheet
+  RSQLite::dbDisconnect(data_sqlite) # closes connection
+  hnames <- get_names(FilePath, sep) # call get_names function (see below) to prepare site and hive# fields to be appended to data tibble
   out <- full_join(dat, hnames, by = "UUID") %>%
     select(Unix_Time, Site, Hive, ScaleID, RecordType, Weight) %>%
     as.tibble() %>%
-    filter(RecordType == "Logged_Data") %>%
-    mutate(TimeStamp = anytime::anytime(Unix_Time, tz = tz)) %>%
-    arrange(ScaleID, TimeStamp) %>%
-    mutate(Weight_despiked = Reduce(c, tapply(Weight, ScaleID, clean_spikes))) %>%
-    filter(TimeStamp > ftrim & TimeStamp < btrim) %>%
+    filter(RecordType == "Logged_Data") %>% # use only logged data readings
+    mutate(TimeStamp = anytime::anytime(Unix_Time, tz = tz)) %>% # convert Unix_Time to human-readable time stamp field
+    arrange(ScaleID, TimeStamp) %>% # arrange chronologically within each device
+    mutate(Weight_despiked = Reduce(c, tapply(Weight, ScaleID, clean_spikes))) %>% # call clean_spikes function (see below) to create Weight_despiked field
+    filter(TimeStamp > ftrim & TimeStamp < btrim) %>% # trim data to desired start and end dates
     select(TimeStamp, Unix_Time, ScaleID, Site, Hive, Weight, Weight_despiked) %>%
-    mutate(Site = as.character(Site))
+    mutate(Site = as.character(Site)) # convert Site field to character so that empty factor levels do not persist after filtering
   return(out)
 }
 
-#' Split HiveName field into Site and Hive#; assumes HiveName follows format site.#
+#' \code{get_names} splits HiveName field into Site and Hive#, assuming HiveName follows format site % sep % hive#
 #'
 #' @param FilePath Path to BroodMinder SQLite database
+#' @param sep A string denoting the separator between the site and hive number in the HiveName field; assumes HiveName follows format site % sep % hive#
 #' @return A tibble consisting of fields UUID, ScaleID, Site, Hive, filtered to contain only unique rows
 #' @export
-get_names <- function(FilePath) {
-  data_sqlite <- RSQLite::dbConnect(RSQLite::SQLite(), FilePath)
-  readings <- RSQLite::dbGetQuery(data_sqlite, "SELECT * FROM BroodMinderReading")
-  SiteNum <- str_split(readings$HiveName, pattern = "\\.", simplify = TRUE)
-  RSQLite::dbDisconnect(data_sqlite)
-  names <- bind_cols(select(readings, Address, HiveName), SiteNum) %>%
-    distinct()
-  colnames(names) <- c("UUID", "ScaleID", "Site", "Hive")
+get_names <- function(FilePath, sep) {
+  data_sqlite <- RSQLite::dbConnect(RSQLite::SQLite(), FilePath) # open db connection
+  readings <- RSQLite::dbGetQuery(data_sqlite, "SELECT * FROM BroodMinderReading") # extract BroodMinderReading sheet
+  SiteNum <- data.frame(str_split(readings$HiveName, pattern = sep, simplify = TRUE)) # split HiveName field
+  RSQLite::dbDisconnect(data_sqlite) # close db connection
+  names <- bind_cols(select(readings, Address, HiveName), SiteNum) %>% # append split HiveName field to Address and HiveName fields
+    distinct() # deduplicate
+  colnames(names) <- c("UUID", "ScaleID", "Site", "Hive") # rename columns
   return(names)
 }
 
-#' Remove strong additive outliers, i.e. "spikes"
+#' \code{clean_spikes} removes strong additive outliers, i.e. "spikes", using the \code{despike} function from package \code{oce}. Parameters are set so that strong spike values are replaced with a running median of width = 7
 #'
 #' @param x A vector of time-ordered observations
 #' @return An adjusted vector of observations in which spieks have been replaced with running median
@@ -49,7 +51,10 @@ clean_spikes <- function(x) {
   oce::despike(x, reference = "median", n = 1.5, k = 7, replace = "reference")
 }
 
-#' Apply 25 h running mean, loess smoothing, differencing, and scaling in preparation for downstream analysis
+#' \code{data_proc} generates and appends four new fields: a 25 h running mean of the despiekd weights,
+#'   a loess smoothing of the 25-h running mean,
+#'   a first-order difference vector of the loess-smoothed 25-h running mean,
+#'   and a z-normalization of the difference vector.
 #'
 #' @param x A tibble or data frame
 #' @return A tibble with added fields
@@ -64,10 +69,10 @@ data_proc <- function(x) {
   return(out)
 }
 
-#' Apply loess smoothing to a time series
+#' \code{smooth_loess} applies loess smoothing to a time series
 #'
 #' @param x A vector of time-ordered observations
-#' @param span A real number indicating the width of the loess smoothing window
+#' @param span A real number indicating the width (proportion of time series observations) of the loess smoothing window
 #' @return A loess-smoothed vector of time-ordered observations
 #' @export
 smooth_loess <- function(x, span = 0.1) {
